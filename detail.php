9f05717623e47b505559393320999a233e175f2c
urnotmiaomiao <60200863+urnotmiaomiao@users.noreply.github.com>
2020-12-04 21:23:45 -0600

    Update detail.php

--- detail.php
+++ detail.php
@@ -21,8 +21,8 @@
         }
 
         .item-img {
-            float: left;
-            width: 200px;
+            /* float: left; */
+            max-width: 300px;
             padding-right: 1rem;
         }
     </style>
@@ -39,62 +39,64 @@ if (mysqli_connect_errno($conn)) {
     die('Connect Error (' . $conn->connect_errno . ') '
         . $conn->connect_error);
 }
-$sql = "SELECT Name, Price, cart_own_product.Num  FROM cart_own_product, product WHERE cart_own_product.AccountID=$account_id AND product.ProductID = cart_own_product.ProductID";
+$sql = "SELECT * FROM product WHERE ProductID=$pid";
 $result = mysqli_query($conn, $sql);
 if ($result != null) {
-    
-}
+    $row = $result->fetch_assoc();
 ?>
 
-<body>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <a class="navbar-brand" href="#">Coffee & Tea</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <body>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <a class="navbar-brand" href="#">Coffee & Tea</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php#coffee-row">Coffee</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php#tea-row">Tea</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php#">About</a>
-                </li>
-            </ul>
-            <a class="btn btn-outline-success my-2 my-sm-0" href="login.html">Login</a>
-        </div>
-    </nav>
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php#coffee-row">Coffee</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php#tea-row">Tea</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php#">About</a>
+                    </li>
+                </ul>
+                <a class="btn btn-outline-success my-2 my-sm-0" href="login.html">Login</a>
+            </div>
+        </nav>
 
-    <main>
-        <div class="container">
-            <h1>Detail Page</h1>
-            <div class="row" id="coffee-row">
-                <div class="col-md-4">
-                    <h2>Coffee#1</h2>
-                    <img class="item-img" src="img/me.jpg" alt="me">
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
-                        mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
-                        magna mollis euismod. Donec sed odio dui. </p>
-                    <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+        <main>
+            <div class="container">
+                <h1>Detail Page</h1>
+                <div class="row justify-content-center" id="coffee-row">
+                    <div class="col-md-4">
+                        <img class="item-img" src="img/<?php echo $row["Image"]; ?>" alt="<?php echo $row["Image"]; ?>">
+                    </div>
+                    <div class="col-md-4">
+                        <h2><?php echo $row["Name"]; ?></h2>
+                        <p><?php echo $row["Description"]; ?></p>
+                        <p><a class="btn btn-secondary" href="#" role="button">Add to Cart</a></p>
+                    </div>
                 </div>
             </div>
-        </div>
-    </main>
-    <footer class="container">
-        <hr>
-        <p>Web Final Project - 2020 Fall - CS6314.002</p>
-    </footer>
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+        </main>
+        <footer class="container">
+            <hr>
+            <p>Web Final Project - 2020 Fall - CS6314.002</p>
+        </footer>
+        <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
-</body>
+    </body>
+<?php
+}
+?>
 
 </html>
