092ae6c96d5971e7035a0e1510f5c3447ff31505
urnotmiaomiao <60200863+urnotmiaomiao@users.noreply.github.com>
2020-12-06 10:40:26 -0600

    Update update.php

--- update.php
+++ update.php
@@ -31,42 +31,60 @@ if ($result != null) {
             <div class="container">
                 <h1>Update item</h1>
                 <div class="row justify-content-center" id="coffee-row">
-                    <form action="updatedatabase.php" method="post" class="col-md-4">
-                        <label for="id">Product ID (not editable): </label>
-                        <input readonly name="id" id="id" value=<?php echo $pid?>>
+                    <div class="col-md-6">
+                        <form action="updatedatabase.php" method="post">
+                            <div class="form-group">
+                                <label for="id">Product ID (not editable): </label>
+                                <input readonly name="id" class="form-control" id="id" value=<?php echo $pid ?>>
+                            </div>
+                            <div class="form-group">
+                                <label for="newImage">Product image file name: </label>
+                                <!-- <img class="item-img" src="img/<?php echo $row["Image"]; ?>" alt="<?php echo $row["Image"]; ?>"> -->
+                                <input name="newImage" class="form-control" id="newImage" value=<?php echo $row["Image"]; ?>>
+                            </div>
 
-                        <label for="newImage">Product image file name: </label>
-                        <img class="item-img" src="img/<?php echo $row["Image"]; ?>" alt="<?php echo $row["Image"]; ?>">
-                        <input name="newImage" id="newImage" value=<?php echo $row["Image"]; ?>>
+                            <div class="form-group">
+                                <label for="newName">Product name: </label>
+                                <!-- <p><?php echo $row["Name"]; ?></p> -->
+                                <input name="newName" class="form-control" id="newName" value=<?php echo $row["Name"]; ?>>
+                            </div>
 
-                        <label for="newName">Product name: </label>
-                        <p><?php echo $row["Name"]; ?></p>
-                        <input name="newName" id="newName" value=<?php echo $row["Name"]; ?>>
+                            <div class="form-group">
+                                <label for="newDescription">Product description: </label>
+                                <!-- <p><?php echo $row["Description"]; ?></p> -->
+                                <textarea name="newDescription" class="form-control" id="newDescription"><?php echo $row["Description"]; ?></textarea>
+                            </div>
 
-                        <label for="newDescription">Product description: </label>
-                        <p><?php echo $row["Description"]; ?></p>
-                        <textarea name="newDescription" id="newDescription"><?php echo $row["Description"]; ?></textarea>
-                        
-                        <label for="newNum">Product current amount: </label>
-                        <p><?php echo $row["Num"]; ?></p>
-                        <input type="number" min="0" step="1" name="newNum" id="newNum" value=<?php echo $row["Num"]; ?>>
-                        
-                        <label for="newCategory">Product category: </label>
-                        <p><?php echo $row["Category"]; ?></p>                        
-                        <select id="newCategory" name="newCategory">
-                            <option value="coffee">Coffee</option>
-                            <option value="tea">Tea</option>
-                            <option selected="selected"><?php echo $row["Category"]; ?></option>
-                        </select>
+                            <div class="form-group"><label for="newNum">Product current amount: </label>
+                                <!-- <p><?php echo $row["Num"]; ?></p> -->
+                                <input type="number" min="0" step="1" name="newNum" class="form-control" id="newNum" value=<?php echo $row["Num"]; ?>>
+                            </div>
 
-                        <label for="newPrice">Product price: </label>
-                        <p><?php echo $row["Price"]; ?></p>
-                        <input type="number" name="newPrice" id="newPrice"value=<?php echo $row["Price"]; ?>>
-                        <input type="submit" value="Submit">
+                            <div class="form-group"><label for="newCategory">Product category: </label>
+                                <!-- <p><?php echo $row["Category"]; ?></p> -->
+                                <select id="newCategory" class="form-control" name="newCategory">
+                                    <option value="coffee">Coffee</option>
+                                    <option value="tea">Tea</option>
+                                    <option selected="selected"><?php echo $row["Category"]; ?></option>
+                                </select>
+                            </div>
 
-                    </form>
+                            <div class="form-group">
+                                <label for="newPrice">Product price: </label>
+                                <!-- <p><?php echo $row["Price"]; ?></p> -->
+                                <input type="number" name="newPrice" class="form-control" id="newPrice" value=<?php echo $row["Price"]; ?>>
+                            </div>
+                            <input type="submit" class="btn btn-primary" value="Submit">
+
+                        </form>
+                    </div>
                 </div>
-                <p><a class="btn btn-secondary" href="index.php">Back</a></p>
+
+                <div class="row justify-content-start">
+                    <div class="col-md-4">
+                        <a class="btn btn-secondary" href="index.php">Back</a>
+                    </div>
+                </div>
             </div>
         </main>
         <footer class="container">
@@ -79,7 +97,7 @@ if ($result != null) {
 
     </body>
 <?php
-$conn->close();
+    $conn->close();
 }
 ?>
 
