9c246b20a22c23c975d340f2289deebd0d549fb9
urnotmiaomiao <60200863+urnotmiaomiao@users.noreply.github.com>
2020-12-05 21:39:35 -0600

    Create header.php

---
+++ partial/header.php
@@ -0,0 +1,26 @@
+<head>
+    <!-- Required meta tags -->
+    <meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Coffee & Tea Distributor">
+    <meta name="keywords" content="Web Project, Coffee & Tea Distributor">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/index.css" />
+    <script src="js/index.js"></script>
+
+    <title>Web Final Project</title>
+    <style>
+        body {
+            padding-top: 3.5rem;
+        }
+
+        .item-img {
+            /* float: left; */
+            max-width: 300px;
+            padding-right: 1rem;
+        }
+    </style>
+</head>
